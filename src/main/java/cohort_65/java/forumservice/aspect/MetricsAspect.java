11e663260ff8b13cd8b3ec6a6efe99b4d673eb77
galin <galin-ka1@hotmail.com>
2025-10-09 10:14:05 +0200

    security JWT with AOP plus idea from Andrej

--- src/main/java/cohort_65/java/forumservice/aspect/MetricsAspect.java
+++ src/main/java/cohort_65/java/forumservice/aspect/MetricsAspect.java
@@ -1,5 +1,7 @@
 package cohort_65.java.forumservice.aspect;
 
+import cohort_65.java.forumservice.accounting.dto.UserRegisterDto;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
@@ -18,7 +20,7 @@ public class MetricsAspect {
         return trackExecution(joinPoint);
     }
 
-    @Around("execution(* cohort_65.java.forumservice.security.service*.*(..))")
+    @Around("execution(* cohort_65.java.forumservice.*.service*.*.*(..))")
     public Object trackSecurityMetrics(ProceedingJoinPoint joinPoint) throws Throwable {
         return trackExecution(joinPoint);
     }
@@ -32,6 +34,19 @@ public class MetricsAspect {
         System.out.println("Time: " + LocalDateTime.now() + " [METRICS] Method " + methodName +
                 " execution time: " + (endTime - startTime) + " ms");
         return result;
+    }
+
+    @Before("execution(* cohort_65.java.forumservice.accounting.service.UserAccountServiceImpl.register(..))")
+    public void validateArgsForRegister(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        UserRegisterDto userRegisterDto = (UserRegisterDto) args[0];
+        if (userRegisterDto.getPassword().length() < 8) {
+            throw new IllegalArgumentException("Password must be at least 8 characters long");
+        }
+        if (!userRegisterDto.getPassword().contains("!")) {
+            throw new IllegalArgumentException("Password must not contain '!' character");
+        }
+        System.out.println("[VALIDATION] Passed validation for register");
     }
 
     @Pointcut("execution(* cohort_65.java.forumservice.post.service.*.*(..))")
